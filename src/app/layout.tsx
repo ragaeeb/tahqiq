436e0e944894c1b396117b655def830db2e14b53
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2025-05-13 13:35:56 -0400

    Fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,32 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from 'next/font/google';
 
-import "./globals.css";
+import './globals.css';
 
 const geistSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-geist-sans",
+    subsets: ['latin'],
+    variable: '--font-geist-sans',
 });
 
 const geistMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-geist-mono",
+    subsets: ['latin'],
+    variable: '--font-geist-mono',
 });
 
 export const metadata: Metadata = {
-  description: "tahqiq",
-  title: "Tahqiq",
+    description: 'tahqiq',
+    title: 'Tahqiq',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+        </html>
+    );
 }
