83218e9964d7b548761be9bd5ff704d99dc60caf
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2025-07-18 18:49:19 -0400

    Fix build

--- src/lib/manuscript.test.ts
+++ src/lib/manuscript.test.ts
@@ -5,49 +5,40 @@ import { mapManuscriptToJuz } from './manuscript';
 describe('manuscript', () => {
     describe('mapManuscriptToJuz', () => {
         it('should convert HH:MM:SS format correctly', () => {
+            const sheets = [
+                {
+                    alt: [],
+                    observations: [
+                        {
+                            bbox: { height: 1, width: 2, x: 3, y: 4 },
+                            id: 1,
+                            isCentered: true,
+                            isHeading: true,
+                            isPoetic: true,
+                            lastUpdate: 2,
+                            text: 'T',
+                        },
+                        {
+                            bbox: { height: 1, width: 1, x: 1, y: 1 },
+                            id: 1,
+                            isFootnote: true,
+                            lastUpdate: 2,
+                            text: 'F',
+                        },
+                    ],
+                    page: 2,
+                },
+            ];
+
             const actual = mapManuscriptToJuz({
                 idsFilter: new Set(),
-                sheets: [
-                    {
-                        alt: [],
-                        observations: [
-                            {
-                                bbox: { height: 1, width: 2, x: 3, y: 4 },
-                                id: 1,
-                                isCentered: true,
-                                isHeading: true,
-                                isPoetic: true,
-                                lastUpdate: 2,
-                                text: 'T',
-                            },
-                            {
-                                bbox: { height: 1, width: 1, x: 1, y: 1 },
-                                id: 1,
-                                isFootnote: true,
-                                lastUpdate: 2,
-                                text: 'F',
-                            },
-                        ],
-                        page: 2,
-                    },
-                ],
+                isInitialized: true,
+                sheets,
             });
 
             expect(actual).toEqual({
-                contractVersion: 'v1.0',
-                index: [
-                    {
-                        page: 2,
-                        title: 'T',
-                    },
-                ],
-                sheets: [
-                    {
-                        footnotes: 'F',
-                        page: 2,
-                        text: 'T',
-                    },
-                ],
+                contractVersion: 'v2.0',
+                sheets,
                 timestamp: expect.any(Date),
                 type: 'juz',
             });
